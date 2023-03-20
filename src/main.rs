b084eae02034439807fc86838c91812b1ec0dbb2
John Spray <jcs@redpanda.com>
2023-03-20 15:28:43 +0000

    Enable setting message size on command line

--- src/main.rs
+++ src/main.rs
@@ -98,6 +98,7 @@ struct ProducerStats {
     errors: u32,
 }
 
+#[derive(Clone)]
 struct Payload {
     compressible: bool,
     min_size: usize,
@@ -186,7 +187,7 @@ async fn producers(
     n: usize,
     properties: Vec<String>,
     compression_type: Option<String>,
-    compressible_payload: bool,
+    payload: Payload,
     timeout: Duration,
 ) {
     let mut tasks = vec![];
@@ -211,11 +212,7 @@ async fn producers(
             i,
             m,
             cfg_pairs,
-            Payload {
-               compressible: compressible_payload,
-               min_size: 16384,
-               max_size: 16384,
-            },
+            payload.clone(),
             timeout,
         )))
     }
@@ -411,6 +408,10 @@ enum Commands {
         #[clap(short, long)]
         compression_type: Option<String>,
         #[clap(short, long)]
+        min_record_size: Option<usize>,
+        #[clap(short, long, default_value_t = 16384)]
+        max_record_size: usize,
+        #[clap(short, long)]
         compressible_payload: bool,
     },
     /// Creates consumer swarm
@@ -448,8 +449,12 @@ async fn main() {
             properties,
             compression_type,
             compressible_payload,
+            min_record_size,
+            max_record_size,
             timeout_ms,
         }) => {
+            let min_size = min_record_size.unwrap_or(*max_record_size);
+
             producers(
                 brokers,
                 topic.clone(),
@@ -457,7 +462,11 @@ async fn main() {
                 *count,
                 properties.clone(),
                 (*compression_type).clone(),
-                compressible_payload.clone(),
+                Payload {
+                    compressible: *compressible_payload,
+                    min_size,
+                    max_size: *max_record_size,
+                },
                 Duration::from_millis(*timeout_ms),
             )
             .await;
